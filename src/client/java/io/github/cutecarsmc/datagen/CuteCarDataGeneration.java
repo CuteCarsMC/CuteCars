304213c55f150165e715210c264f41c7921e0a3f
Auri <f5bde6@proton.me>
2026-04-16 01:01:00 +0100

    Datagen entrypoint

---
+++ src/client/java/io/github/cutecarsmc/datagen/CuteCarDataGeneration.java
@@ -0,0 +1,13 @@
+package io.github.cutecarsmc.datagen;
+
+import dev.kikugie.fletching_table.annotation.fabric.Entrypoint;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+@Entrypoint("fabric-datagen")
+public class CuteCarDataGeneration implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
