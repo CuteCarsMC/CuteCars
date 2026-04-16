625c4bdbbeaeea9f89132ccc03f7c532a7ffd441
esoij <Dariuspotup@gmail.com>
2026-04-15 18:34:23 -0600

    final

--- src/main/java/io/github/cutecarsmc/CuteCarsMain.java
+++ src/main/java/io/github/cutecarsmc/CuteCarsMain.java
@@ -25,7 +25,7 @@ import dev.kikugie.fletching_table.annotation.fabric.Entrypoint;
 import net.fabricmc.api.ModInitializer;
 
 @Entrypoint("main")
-public class CuteCarsMain implements ModInitializer {
+public final class CuteCarsMain implements ModInitializer {
 	@Override
 	public void onInitialize() {
 	}
