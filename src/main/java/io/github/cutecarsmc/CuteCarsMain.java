5a0289cfea283ec79a2f3cf62c2c35f9e5f2498b
esoij <Dariuspotup@gmail.com>
2026-04-15 18:22:14 -0600

    add main file, only doing this in this commit so that others dont do it and it ends up being a whole mess and it sucks and everyone dies

---
+++ src/main/java/io/github/cutecarsmc/CuteCarsMain.java
@@ -0,0 +1,11 @@
+package io.github.cutecarsmc;
+
+import dev.kikugie.fletching_table.annotation.fabric.Entrypoint;
+import net.fabricmc.api.ModInitializer;
+
+@Entrypoint("main")
+public class CuteCarsMain implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
