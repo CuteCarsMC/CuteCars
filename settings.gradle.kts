260a69019826952b7cc3fe03deb3af7e4ca09ce0
p1k0chu <p1k0chu.vim@gmail.com>
2026-04-17 03:37:14 +0300

    Add java/kt to EditorConfig (#10)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,33 +1,33 @@
 pluginManagement {
-	repositories {
-		mavenCentral()
-		gradlePluginPortal()
-		maven("https://maven.fabricmc.net")
-		maven("https://maven.architectury.dev")
-		maven("https://maven.kikugie.dev/snapshots")
-		maven("https://maven.kikugie.dev/releases")
-		maven("https://repo.polyfrost.cc/releases")
-	}
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.fabricmc.net")
+        maven("https://maven.architectury.dev")
+        maven("https://maven.kikugie.dev/snapshots")
+        maven("https://maven.kikugie.dev/releases")
+        maven("https://repo.polyfrost.cc/releases")
+    }
 }
 
 plugins {
-	id("dev.kikugie.stonecutter") version "0.8.3"
+    id("dev.kikugie.stonecutter") version "0.8.3"
 }
 
 stonecutter {
-	kotlinController = true
-	centralScript = "build.gradle.kts"
-	create(rootProject) {
-		fun mc(mcVersion: String, loaders: Iterable<String>) {
-			for (loader in loaders) {
+    kotlinController = true
+    centralScript = "build.gradle.kts"
+    create(rootProject) {
+        fun mc(mcVersion: String, loaders: Iterable<String>) {
+            for (loader in loaders) {
                 version("$mcVersion-$loader", mcVersion)
-			}
-		}
+            }
+        }
 
-		mc("26.1", listOf("fabric"))
+        mc("26.1", listOf("fabric"))
 
-		vcsVersion = "26.1-fabric"
-	}
+        vcsVersion = "26.1-fabric"
+    }
 }
 
 dependencyResolutionManagement {
